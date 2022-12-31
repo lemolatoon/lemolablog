4e94f62d5b5694e8b6e183161dd2795860cebef7
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-12-31 16:28:06 +0900

    inspect og.tsx

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ImageResponse } from "@vercel/og";
 import { NextApiRequest } from "next";
 import styled from "styled-components";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import lemolablogImage from "public/lemolablog.png";
 import Image from "next/image";
 
@@ -9,6 +10,7 @@ export const config = {
   runtime: "experimental-edge",
 };
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const AbsoluteTitle = styled.div`
   position: absolute;
   max-width: 90%;
@@ -20,6 +22,7 @@ const AbsoluteTitle = styled.div`
   z-index: 999;
 `;
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const AbsoluteImage = styled(Image)`
   position: absolute;
   left: 0;
@@ -32,10 +35,11 @@ const AbsoluteImage = styled(Image)`
 export default function handler(_req: NextApiRequest) {
   return new ImageResponse(
     (
-      <div>
-        <AbsoluteTitle>Test Title</AbsoluteTitle>
-        <AbsoluteImage src={lemolablogImage} alt="lemolablog" height={80} />
-      </div>
+      <h1>
+        {/* <AbsoluteTitle>Test Title</AbsoluteTitle>
+        <AbsoluteImage src={lemolablogImage} alt="lemolablog" height={80} /> */}
+        HELLO WORLD
+      </h1>
     )
   );
 }
