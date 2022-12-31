7c811700573b10cddf98c32b02765c75e181d07d
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-12-31 22:49:59 +0900

    Add twitter support (ogp)

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -5,13 +5,18 @@ type MetaProps = {
   title: string;
 };
 export const HeadsForPost = ({ title }: MetaProps) => {
+  const imageUrl = `https://lemolablog.vercel.app/api/og?title=${title}`;
   return (
     <Head>
       <title>{title}</title>
+      <meta property="og:image" content={imageUrl} />
+      <meta name="twitter:card" content="summary_large_image"></meta>
       <meta
-        property="og:image"
-        content={`https://lemolablog.vercel.app/api/og?title=${title}`}
-      />
+        name="twitter:domain"
+        content="https://lemolablog.vercel.app"
+      ></meta>
+      <meta name="twitter:image" content={imageUrl}></meta>
+      <meta name="twitter:title" content="@lemolatoon1 のblog"></meta>
     </Head>
   );
 };
