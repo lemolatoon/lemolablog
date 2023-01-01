e0685e0fa5dd8da74d3a4e5791c0f6f8ca04a35e
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-01-01 20:27:17 +0900

    add try catch

--- pages/api/revalidate_posts.ts
+++ pages/api/revalidate_posts.ts
@@ -5,31 +5,39 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const body = JSON.parse(req.body);
-  console.log("HERE IS BODY.");
-  console.log(req);
-  console.log(body);
-  console.log(body);
-  // Check for secret to confirm this is a valid request
-  const { secret } = req.query;
-  if (!secret || req.query.secret !== process.env.REVALIDATE_SECRET_TOKEN) {
-    console.error("Invalid token.");
-    console.error("got:");
-    console.error(secret);
-    console.error("expected:");
-    console.error(process.env.REVALIDATE_SECRET_TOKEN);
-    return res.status(401).json({ message: "Invalid token" });
-  }
-
-  return res.status(200).json({ message: "ok" });
   try {
-    // this should be the actual path not a rewritten path
-    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-    await res.revalidate("/path-to-revalidate");
-    return res.json({ revalidated: true });
-  } catch (err) {
-    // If there was an error, Next.js will continue
-    // to show the last successfully generated page
-    return res.status(500).send("Error revalidating");
+    console.log("HERE IS BODY.");
+    console.log(req);
+    console.log(req.body);
+    const body = JSON.parse(req.body);
+    console.log(body);
+    console.log(body);
+    // Check for secret to confirm this is a valid request
+    const { secret } = req.query;
+    if (!secret || req.query.secret !== process.env.REVALIDATE_SECRET_TOKEN) {
+      console.error("Invalid token.");
+      console.error("got:");
+      console.error(secret);
+      console.error("expected:");
+      console.error(process.env.REVALIDATE_SECRET_TOKEN);
+      return res.status(401).json({ message: "Invalid token" });
+    }
+
+    return res.status(200).json({ message: "ok" });
+    try {
+      // this should be the actual path not a rewritten path
+      // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+      await res.revalidate("/path-to-revalidate");
+      return res.json({ revalidated: true });
+    } catch (err) {
+      // If there was an error, Next.js will continue
+      // to show the last successfully generated page
+      return res.status(500).send("Error revalidating");
+    }
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(e.message);
+    }
+    console.error("moro morro failed");
   }
 }
