d32892084acf853503b55f097dfc92b15e06cec1
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-01-01 18:01:22 +0900

    Add revalidate api route

---
+++ pages/api/revalidate_posts.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+// https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration#using-on-demand-revalidation
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  console.log("HERE IS BODY.");
+  console.log(req.body);
+  // Check for secret to confirm this is a valid request
+  const { secret } = req.query;
+  if (!secret || req.query.secret !== process.env.REVALIDATE_SECRET_TOKEN) {
+    console.error("Invalid token.");
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  return res.status(200).json({ message: "ok" });
+  try {
+    // this should be the actual path not a rewritten path
+    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+    await res.revalidate("/path-to-revalidate");
+    return res.json({ revalidated: true });
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send("Error revalidating");
+  }
+}
--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -9,3 +9,29 @@ export type Post = {
   updated_at: string;
   published_at: string;
 };
+
+// eslint-disable-next-line
+type TableRecord<T> = any;
+
+export type InsertPayload<T> = {
+  type: "INSERT";
+  table: string;
+  schema: string;
+  record: TableRecord<T>;
+  old_record: null;
+};
+
+export type UpdatePayload<T> = {
+  type: "UPDATE";
+  table: string;
+  schema: string;
+  record: TableRecord<T>;
+  old_record: TableRecord<T>;
+};
+export type DeletePayload<T> = {
+  type: "DELETE";
+  table: string;
+  schema: string;
+  record: null;
+  old_record: TableRecord<T>;
+};
